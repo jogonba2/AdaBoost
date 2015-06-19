8d51fa8913a524b94da9a26828739983a7577780
Overxfl0w13 <jogonba2@inf.upv.es>
2015-06-19 02:20:28 +0200

    Finished project!

--- AdaBoost.py
+++ AdaBoost.py
@@ -11,12 +11,32 @@ from Classifiers import Multinomial
 from Classifiers import Perceptron
 from Classifiers import KernelPerceptron
 from math import log,floor,e
+from os import system,name
 try: import cPickle as pickle
 except: import pickle
 from sys import argv
 
-# Traducir #
-# Implementar gestion de arguments #
+def cls(): system(['clear','cls'][name == 'nt'])
+    
+def header():
+	print """
+   _   _   _   _   _   _   _   _  
+  / \ / \ / \ / \ / \ / \ / \ / \ 
+ ( A | d | a | B | o | o | s | t )
+  \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ 
+          By Overxfl0w13
+"""
+
+def footer(result_file=None): print "[END] Process finished without saving results.\n" if result_file==None else "[END] Process finished, saved classified in file "+result_file+". \n"
+
+def usage():
+	print "Usage: AdaBoost.py train_data_file iterations [classify] [test_data_file] [output_file] \n\n\
+\ttrain_data_file -> Name of file with train data\n\
+\titerations      -> process iterations\n\
+\tclassify        -> Optional [YES-NO], specifies if you want to classify test data\n\
+\ttest_data_file  -> Optional, only if you want to classify, specifies name of file with test data\n\
+\toutput_file     -> Optional, specifies destination file\n"
+
 def AdaBoost(samples,M):
 	weight_samples    = [1.0/len(samples) for sample in samples]
 	classifiers       = [K_nearest_neighbour,Multinomial,Perceptron,KernelPerceptron]
@@ -26,14 +46,14 @@ def AdaBoost(samples,M):
 		best_classifier       = K_nearest_neighbour # Random #
 		index_best_classifier = 0 # Random #
 		index_sample          = 0
-		calculated_classes = [[] for classifier in classifiers]
+		computed_classes = [[] for classifier in classifiers]
 		for sample in samples:
 			cclass = sample[1]
 			sample = sample[0]
 			index_classifier = 0
 			for classifier in classifiers:
 				computed_class = classifier.classify(samples,sample)
-				calculated_classes[index_classifier].append(computed_class)
+				computed_classes[index_classifier].append(computed_class)
 				if computed_class != cclass: classifiers_error[index_classifier] += weight_samples[index_sample]
 				index_classifier += 1
 			index_sample += 1
@@ -46,13 +66,13 @@ def AdaBoost(samples,M):
 		# Configurar clasificador de la iteracion actual #
 		final_classifier.append((alpha_best_classifier,best_classifier))
 		# Si el error > 0.5 parar #
-		if min_error>0.5 or min_error==0:  print "Mejor error con los clasificadores por separado.\n";  return final_classifier
+		if min_error>0.5 or min_error==0:  print "[!] Min error with only 1 classifier.\n";  return final_classifier
 		# Recalcular pesos de las muestras #
 		index_sample = 0
 		for sample in samples:
 			cclass = sample[1]
 			sample = sample[0]
-			weight_samples[index_sample] = weight_samples[index_sample]*(e**(-cclass*alpha_best_classifier*calculated_classes[index_best_classifier][index_sample]))
+			weight_samples[index_sample] = weight_samples[index_sample]*(e**(-cclass*alpha_best_classifier*computed_classes[index_best_classifier][index_sample]))
 		# Normalizar pesos de las muestras #
 		index_sample  = 0
 		total_weights = sum(weight_samples) 
@@ -69,26 +89,51 @@ def save_object(object,dest):
 		pickle.dump(object,fd,pickle.HIGHEST_PROTOCOL)
 	fd.close()	
 	
-def classify(final_classifier,samples,sample):
+def classify_boost(final_classifier,samples,sample):
 	val = 0
 	for item in final_classifier: val = item[0]*item[1].classify(samples,sample)
 	return -1 if val<0 else 1
 
+def classify_file(final_classifier,samples,test_samples,output_file):
+	with open(output_file,"w") as fd:	
+		fd.write("""   _   _   _   _   _   _   _  
+  / \ / \ / \ / \ / \ / \ / \ 
+ ( R | e | s | u | l | t | s )
+  \_/ \_/ \_/ \_/ \_/ \_/ \_/\r\n\r\n\r\n""")
+		fd.write(stringify_classifier(final_classifier)+"\r\n")
+  	
+		for sample in test_samples: fd.write("Sample "+str(sample)+" classified in: "+str(classify_boost(final_classifier,samples,sample))+"\r\n")
+	fd.close()
+    	
 def stringify_classifier(final_classifier):  
-	for item in final_classifier:  print item[0],item[1].__str__()
-
-def usage():
-	print "Usage: AdaBoost.py train_data_file iterations [classify] [test_data_file] \n\n\
-\ttrain_data_file -> Name of file with train data\n\
-\titerations      -> process iterations\n\
-\tclassify        -> Optional, specifies if you want to classify test data\n\
-\ttest_data_file  -> Optional, only if you want to classify, specifies name of file with test data\n"
+	st = " -> "
+	for item in final_classifier: st += str(item[0])+"*"+item[1].__str__()+"(x)+"
+	return st[:-1]
+	
+def __str__(final_classifier):
+	print "Classifier\n".center(80)
+	print "----------\n".center(80)
+	st = stringify_classifier(final_classifier)
+	print "\n"+st+"\n"
 	
 if __name__ == "__main__":
+	cls()
+	header()
 	if len(argv)<3: usage();exit(0)
-	train_samples = load_data("flowerdata.dat")
-	#test_samples  = load_data("flowerdata.dat") # Test with same train data, ... VERY OPTIMISTIC!! #
-	final_classifier = AdaBoost(train_samples,3)
-	print classify(final_classifier,train_samples,[1,6.1,1.5,-1337])
-	stringify_classifier(final_classifier)
-
+	elif len(argv)!=6 or argv[3].lower() not in ["yes","no"]: usage();exit()
+	train_data_file = argv[1]
+	iterations      = int(argv[2])
+	if len(argv)>3:
+		classify        = argv[3]
+		test_data_file  = argv[4]
+		output_file     = argv[5]
+		train_samples = load_data(train_data_file)
+	final_classifier = AdaBoost(train_samples,iterations)
+	if len(argv)>=3 and argv[3].lower()=="yes":
+		test_samples  = load_data(test_data_file) # Test with same train data, ... VERY OPTIMISTIC!! #
+		classify_file(final_classifier,train_samples,test_samples,output_file)
+		__str__(final_classifier)
+		footer(output_file)
+	else: 
+		__str__(final_classifier)
+		footer()
